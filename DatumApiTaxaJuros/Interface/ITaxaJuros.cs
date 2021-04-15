a8d1ecd5b6c881fbb32e5ee07dc25e06112ce198
Ingrid Pinto Carneiro <ingpcar@gmail.com>
2021-04-15 17:20:52 -0300

    Upgrade do .NET Core para o . NET 5.0 para suportar a estrutura do projeto de teste de integração.

---
+++ DatumApiTaxaJuros/Interface/ITaxaJuros.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatumApiTaxaJuros.Interface
+{
+    public interface ITaxaJuros
+    {
+        public decimal ValorTaxaJuros { get; }
+    }
+}
